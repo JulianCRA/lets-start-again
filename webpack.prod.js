0deb8364da0e2ef55e4a0e829c4a32542ae2b44a
JulianCRA <contact@julianr.dev>
2020-01-26 23:23:42 +0700

    Modular webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,30 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+const WebpackCdnPlugin = require('webpack-cdn-plugin')
+
+module.exports = merge(common, {
+	mode : 'production',
+
+	output : {
+		publicPath: './'
+	},
+
+	externals : {
+		'p5' : 'p5'
+	},
+
+	plugins : [
+		new WebpackCdnPlugin({
+			modules : [
+				{
+					name: 'p5',
+					var: 'p5',
+				}
+			],
+			prod: true
+		})
+	],
+
+	devtool : "hidden-source-map"
+});
