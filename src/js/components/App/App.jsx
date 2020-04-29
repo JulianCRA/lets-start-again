78145fa32831ed08c87509140242ddb49a80146d
JulianCRA <contact@julianr.dev>
2020-04-29 22:16:21 +0700

    Updated dependencies. Added @babel/plugin-transform-runtime

---
+++ src/js/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from './App.module.css'
+
+const App = () => {
+	const [wtf, setWtf] = React.useState(false)
+
+	const huh = async e => {
+		console.log('e :>> ', e);
+	}
+	return(
+		<div>
+			LOLOLOL
+		</div>
+	)
+}
+
+export default App
--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,8 @@ module.exports = {
 					
 						"plugins" : [
 							["@babel/plugin-proposal-class-properties", { "loose": true }],
-							["@babel/plugin-syntax-class-properties"]
+							["@babel/plugin-syntax-class-properties"],
+							["@babel/transform-runtime"]
 						]
 					}
 				}
